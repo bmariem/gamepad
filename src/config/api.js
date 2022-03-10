9184e13544306a650a1c43e0af2b17403346f02b
bmariem <mariem.borchani@gmail.com>
2022-03-10 23:20:43 +0100

    Add remote API

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000/", // Backend
+  baseURL: "https://gamepad-mbelga.herokuapp.com/", // gamepad Backend
 });
 
 export default instance;
